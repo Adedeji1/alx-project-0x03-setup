844ed0fafc1c4e0b7daf22c89dc533f3ebb50d62
Adedeji1 <afolabiadedeji110@yahoo.com>
2025-11-22 11:26:30 +0100

    addition of brackert

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -5,10 +5,10 @@ interface PageRouteProps {
   pageRoute: string
 }
 
-
 export default function Home() {
-  
-  const router = useRouter()
+
+    const router = useRouter();
+
 
   // Imeperative routing with useRouter
   const routeToNextPage  = ({ pageRoute }: PageRouteProps) => {
